81e985366c1c2a27165664bdb4e3af188be67eea
mancho <marin.marinov0611@gmail.com>
2025-12-23 04:47:51 +0200

    yes kitty meow now

---
+++ DingusTouchHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DingusSpinny
+{
+    public class DingusTouchHandler : MonoBehaviour
+    {
+        public static bool dingusblewuplol = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            DingusStuff();
+        }
+
+        void DingusStuff()
+        {
+            Debug.Log("kaboom");
+            Plugin.dingus.SetActive(false);
+            Plugin.kaboom.SetActive(true);
+            dingusblewuplol = true;
+            Plugin.timer = 0f;
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -2,6 +2,7 @@ using BepInEx;
 using BepInEx.Configuration;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 using UnityEngine;
 
 namespace DingusSpinny
@@ -11,29 +12,80 @@ namespace DingusSpinny
     {
         private AssetBundle bundle;
         private GameObject asset;
-        private GameObject spawnedDingus;
-        private ConfigEntry<float> configSpinSpeed;
+        public static GameObject spawnedDingus;
+        public static GameObject dingus;
+        public static GameObject kaboom;
+        public static ConfigEntry<float> configSpinSpeed;
+        public static float timer = 0f;
 
         void Update()
         {
             if (spawnedDingus != null)
             {
-                spawnedDingus.transform.Rotate(0f, configSpinSpeed.Value * Time.deltaTime, 0f, Space.World);
+                spawnedDingus.transform.Rotate( 0f, configSpinSpeed.Value * Time.deltaTime, 0f, Space.World );
+            }
+            if (UnityInput.Current.GetKeyDown(KeyCode.Backslash))
+            {
+                dingus.SetActive(true);
+                kaboom.SetActive(false);
+                DingusTouchHandler.dingusblewuplol = false;
+            }
+            if (DingusTouchHandler.dingusblewuplol)
+            {
+                timer += Time.deltaTime;
+
+                if (timer >= 10f)
+                {
+                    DingusTouchHandler.dingusblewuplol = false;
+                    dingus.SetActive(true);
+                }
             }
         }
+
         void OnPlayerSpawned()
         {
-            spawnedDingus = Instantiate(asset, new Vector3(-65.7588f, 11.6926f, -80.1358f), Quaternion.Euler(270f, 234f, 0f));
-            Destroy(spawnedDingus.GetComponent<Collider>());
+            SpawnDingus();
         }
+        void SpawnDingus()
+        {
+            spawnedDingus = Instantiate(asset, new Vector3(-65.7588f, 11.6926f, -80.1358f), Quaternion.Euler(0f, 234f, 0f));
+            dingus = spawnedDingus.transform.GetChild(0).gameObject;
+            kaboom = spawnedDingus.transform.GetChild(1).gameObject;
+
+            kaboom.SetActive(false);
+            SetupInteraction(dingus);
+        }
+
         void Start()
         {
             bundle = LoadAssetBundle("DingusSpinny.assets.dingus");
-            asset = bundle.LoadAsset<GameObject>("dingus");
+            asset = bundle.LoadAsset<GameObject>("dingus root");
+
             GorillaTagger.OnPlayerSpawned(OnPlayerSpawned);
 
             configSpinSpeed = Config.Bind("General", "Spin Speed", 90f, "dingus spinny speed yeah!");
         }
+
+        void SetupInteraction(GameObject dingus)
+        {
+            MeshCollider meshCollider = dingus.GetComponent<MeshCollider>();
+            if (meshCollider != null)
+            {
+                meshCollider.convex = true;
+                meshCollider.isTrigger = true;
+            }
+
+            Rigidbody rigidbody = dingus.GetComponent<Rigidbody>();
+            if (rigidbody == null)
+                rigidbody = dingus.AddComponent<Rigidbody>();
+
+            rigidbody.isKinematic = true;
+            rigidbody.useGravity = false;
+
+            if (dingus.GetComponent<DingusTouchHandler>() == null)
+                dingus.AddComponent<DingusTouchHandler>();
+        }
+
         public AssetBundle LoadAssetBundle(string path)
         {
             Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
